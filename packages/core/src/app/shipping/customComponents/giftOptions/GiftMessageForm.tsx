2f4ebe22f155a39cf4e50b37bbbecf2240995482
Matthew Steele <matthewsteeleonline@gmail.com>
2023-08-24 13:03:03 -0500

    Commenting unused import

--- packages/core/src/app/shipping/customComponents/giftOptions/GiftMessageForm.tsx
+++ packages/core/src/app/shipping/customComponents/giftOptions/GiftMessageForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import IconContainer from "../IconContainer";
 import GiftMessageIcon from "../CustomIcon";
 
