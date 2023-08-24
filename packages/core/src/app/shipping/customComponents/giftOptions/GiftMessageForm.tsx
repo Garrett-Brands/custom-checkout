4a98972ee0a6473be523fae8c509dfab507a4b83
Brian N <0xb3rlin@gmail.com>
2023-08-24 12:30:46 -0500

    Gift Message: Set up function for front end validation for limiting to 10 lines and removing special characters.

--- packages/core/src/app/shipping/customComponents/giftOptions/GiftMessageForm.tsx
+++ packages/core/src/app/shipping/customComponents/giftOptions/GiftMessageForm.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import IconContainer from "../IconContainer";
 import GiftMessageIcon from "../CustomIcon";
 
 const GiftMessageForm = (props: any) => {
     const { giftMessage, setGiftMessage } = props
+    const [message, setMessage] = useState('');
+
     const maxLength = 350
     const maxRows = 10
     const placeholder = "Limited to 350 characters and 10 lines, please do not use emojis or special characters. Don't forget to sign your name!"
@@ -13,6 +15,31 @@ const GiftMessageForm = (props: any) => {
             return giftMessage
         }
     }
+
+    // TODO: Replace textarea's onChange with this function
+    const validateAndSanitizeInput = (event) => {
+        const value = event.target.value.replace(/[^a-zA-Z0-9 .,!?]/g, '');
+        const lineCount = (value.match(/\n/g) || []).length;
+        const isInsertingLineBreak = event.nativeEvent.inputType === 'insertLineBreak';
+        const isInsertingCharacter = event.nativeEvent.inputType !== 'deleteContentBackward';
+
+        if (lineCount >= maxRows && isInsertingLineBreak) {
+        setMessage('You have exceeded the maximum line limit.');
+        event.preventDefault();
+        return;
+        }
+
+        if (value.length >= maxLength && isInsertingCharacter) {
+        setMessage('You have reached the maximum character limit.');
+        event.preventDefault();
+
+        return;
+        }
+
+        setMessage('');
+        setGiftMessage(value);
+    };
+    
 
     return(
         <>
