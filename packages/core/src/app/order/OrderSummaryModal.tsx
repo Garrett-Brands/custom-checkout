c1b17ce068a6d47ba8c037dc06d7e3d61a524e96
Matthew Steele <matthewsteeleonline@gmail.com>
2024-11-08 09:08:39 -0600

    Adding dynamic delivery to mobile order summary modal

--- packages/core/src/app/order/OrderSummaryModal.tsx
+++ packages/core/src/app/order/OrderSummaryModal.tsx
@@ -3,7 +3,7 @@ import {
     ShopperCurrency as ShopperCurrencyType,
     StoreCurrency,
 } from '@bigcommerce/checkout-sdk';
-import React, { cloneElement, FunctionComponent, isValidElement, ReactNode, useEffect, useState } from 'react';
+import React, { cloneElement, FunctionComponent, isValidElement, ReactNode, useEffect, useState, useRef } from 'react';
 
 import { preventDefault } from '@bigcommerce/checkout/dom-utils';
 import { TranslatedString } from '@bigcommerce/checkout/locale';
@@ -49,7 +49,44 @@ const OrderSummaryModal: FunctionComponent<
     total,
     ...orderSummarySubtotalsProps
 }) => {
+
+    interface ShipDateObject {
+        formatted: string;
+    }
+
+    interface ShipDateObject {
+        formatted: string;
+        isoDate: string;
+    }
+    
+    interface DeliveryDate {
+        formatted: string;
+    }
+    
+    interface ScheduledShipMethod {
+        shipDate: string;
+        deliveryDate: DeliveryDate;
+    }
+    
+    interface AllScheduledShipMethods {
+        [method: string]: ScheduledShipMethod[];
+    }
+
+    interface isMultiShippingMode {
+        value: string;
+    }
+
     const displayInclusiveTax = isTaxIncluded && taxes && taxes.length > 0;
+    const [formattedShipDate, setFormattedShipDate] = useState<string | null>(null);
+    const [formattedDeliveryEstimate, setFormattedDeliveryEstimate] = useState<string | null>(null);
+    const [zipCode, setZipCode] = useState<string | null>(null);
+    const [checkoutZipCode, setCheckoutZipCode] = useState<string | null>(null);
+    const [addressZipCode, setAddressZipCode] = useState<string | null>(null);
+    const [shippingMethod, setShippingMethod] = useState<string | null>(null);
+    const [isMultiShippingMode, setIsMultiShippingMode] = useState<boolean>(false);
+    const [multiShipClick, setMultiShipClick] = useState<boolean>(false);
+    const [addressClick, setAddressClick] = useState<boolean>(false);
+    console.log(multiShipClick)
 
     const subHeaderText = <OrderModalSummarySubheader
         amountWithCurrency={<ShopperCurrency amount={total} />}
@@ -65,29 +102,110 @@ const OrderSummaryModal: FunctionComponent<
             <TranslatedString id="cart.return_to_checkout" />
     </Button>;
 
-    interface ShipDateObject {
-        formatted: string;
+    function getDeliveryDateFormatted(
+        allScheduledShipMethods: AllScheduledShipMethods | null,
+        shippingMethod: string | null,
+        isoDate: string | null
+    ): string | null {
+        if (
+            allScheduledShipMethods &&
+            shippingMethod &&
+            Array.isArray(allScheduledShipMethods[shippingMethod])
+        ) {
+            const shippingData = allScheduledShipMethods[shippingMethod].find(
+                (entry) => entry.shipDate === isoDate
+            );
+            if (shippingData) {
+                return shippingData.deliveryDate.formatted;
+            }
+        }
+        return null;
     }
 
-    // interface DeliveryEstimateObject {
-    //     formatted: string;
-    // }
+    function getFormattedShippingMethod(method: string): string | null {
+        const validMethods = ["Ground", "Next Day Air", "2nd Day Air"];
+        for (const validMethod of validMethods) {
+            if (method.includes(validMethod)) {
+                return validMethod; // Return the matched method
+            }
+        }
+        return null; // Return null if no valid method is found
+    }
 
-    const [formattedShipDate, setFormattedShipDate] = useState<string | null>(null);
-    // const [formattedDeliveryEstimate, setFormatedDeliveryEstimate] = useState<string | null>(null);
+    const lastZipCodeRef = useRef<string | null>(null);
 
     useEffect(() => {
-    const shipDateObject = localStorage.getItem('selectedShipDateObject');
-    // const deliveryEstimateObject = localStorage.getItem('deliveryEstimateObject');
-    if (shipDateObject && shipDateObject !== 'null') {
-        const parsedShipDateObject: ShipDateObject = JSON.parse(shipDateObject);
-        setFormattedShipDate(parsedShipDateObject.formatted);
+        window.addEventListener('multiShipClick', (event) => {
+            const multiShipClickEvent = event as CustomEvent<{ isMultiShippingMode: boolean }>;
+            console.log(multiShipClickEvent)
+            setMultiShipClick(!multiShipClick)
+        });
+        window.addEventListener('addressClick', (event) => {
+            const addressClickEvent = event as CustomEvent<{ address: Address }>;
+            const eventZipCode = (addressClickEvent.detail.address as unknown as { postalCode: string }).postalCode;
+            setAddressClick(!addressClick)
+            setAddressZipCode(eventZipCode)
+            setCheckoutZipCode(eventZipCode)
+        });
+        window.addEventListener('zipCodeInputEvent', (event) => {
+            const zipCodeInputEvent = event as CustomEvent<{ zipCodeInput: string }>;
+            const zipCodeValue = zipCodeInputEvent.detail.zipCodeInput
+            if (zipCodeValue && zipCodeValue !== lastZipCodeRef.current) {
+                lastZipCodeRef.current = zipCodeValue;
+                setAddressZipCode(zipCodeValue);
+                setCheckoutZipCode(zipCodeValue);
+            }
+        });
+    })
+
+    useEffect(() => {
+        const shipDateObject = localStorage.getItem('selectedShipDateObject');
+        const zipCodeItem = localStorage.getItem('zipCode');
+        const checkoutZipCodeItem = localStorage.getItem('checkoutZipCode')
+        const checkoutShippingMethod = localStorage.getItem('checkoutShippingMethod')
+        const isMultiShippingModeItem = localStorage.getItem('isMultiShippingMode')
+        const allScheduledShipMethods: AllScheduledShipMethods | null = JSON.parse(
+            localStorage.getItem('allScheduledShipMethods') || 'null'
+        );
+        
+        if (shipDateObject && shipDateObject !== 'null') {
+            const parsedShipDateObject: ShipDateObject = JSON.parse(shipDateObject);
+            const deliveryDate = getDeliveryDateFormatted(allScheduledShipMethods, shippingMethod, parsedShipDateObject.isoDate)
+            setFormattedShipDate(parsedShipDateObject.formatted);
+            setFormattedDeliveryEstimate(deliveryDate);
+        }
+        if (zipCodeItem && zipCodeItem !== 'null' && checkoutZipCodeItem && checkoutZipCodeItem !== 'null' && checkoutZipCodeItem !== undefined) {
+            const rawZipCode = localStorage.getItem("zipCode");
+            const zipCodeItem = rawZipCode ? JSON.parse(rawZipCode) : null;
+            // const rawCheckoutZipCode = localStorage.getItem("checkoutZipCode");
+            // const checkoutZipCodeItem = rawCheckoutZipCode ? JSON.parse(rawCheckoutZipCode) : null;
+            setZipCode(zipCodeItem);
+            // setCheckoutZipCode(checkoutZipCodeItem)
+        }
+        if (checkoutShippingMethod && checkoutShippingMethod !== 'null') {
+            const rawShippingMethod = localStorage.getItem("checkoutShippingMethod");
+            const rawShippingMethodItem = rawShippingMethod ? JSON.parse(rawShippingMethod) : null;
+            setShippingMethod(getFormattedShippingMethod(rawShippingMethodItem));
+        }
+        if (isMultiShippingModeItem !== null && isMultiShippingModeItem !== 'null') {
+            const parsedIsMultiShipModeObject: isMultiShippingMode = JSON.parse(isMultiShippingModeItem);
+            if (typeof parsedIsMultiShipModeObject.value === "boolean") {
+                setIsMultiShippingMode(parsedIsMultiShipModeObject.value);
+            }
+        }
+    }), [isMultiShippingMode, zipCode, checkoutZipCode, shippingMethod, formattedShipDate, formattedDeliveryEstimate];
+
+    const renderEstimatedDelivery = () => {
+        if (zipCode === checkoutZipCode || zipCode === addressZipCode) {
+            return (
+                <div className="shipping-preview-item" data-type="delivery-date">
+                    <span>Estimated Delivery</span>
+                <span>{formattedDeliveryEstimate}</span>
+            </div>
+            );
+        }
+        return;
     }
-    // if (deliveryEstimateObject && deliveryEstimateObject !== 'null') {
-    //     const parsedDeliveryEstimateObject: DeliveryEstimateObject = JSON.parse(deliveryEstimateObject);
-    //     setFormatedDeliveryEstimate(parsedDeliveryEstimateObject.formatted);
-    //     }
-    }, []); // Empty dependency array ensures this only runs once when the component mounts
 
     return <Modal
         additionalBodyClassName="cart-modal-body optimizedCheckout-orderSummary"
@@ -106,17 +224,15 @@ const OrderSummaryModal: FunctionComponent<
         <OrderSummarySection>
             <OrderSummaryItems displayLineItemsCount={false} items={items} />
         </OrderSummarySection>
-        { formattedShipDate && 
+        {formattedShipDate && 
                 <OrderSummarySection>
                     <div className="shipping-preview-container-orderSummary">
                         <div className="shipping-preview-item" data-type="ship-date">
                             <span>Ship Date</span>
                             <span>{formattedShipDate}</span>
                         </div>
-                        {/* <div className="shipping-preview-item" data-type="delivery-date">
-                            <span>Est. Delivery</span>
-                            <span>{formattedDeliveryEstimate}</span>
-                        </div> */}
+                        { isMultiShippingMode === false && renderEstimatedDelivery()}
+                        {/* <span>values: {zipCode} {checkoutZipCode} {addressZipCode}</span> */}
                     </div>
                 </OrderSummarySection>
         }
